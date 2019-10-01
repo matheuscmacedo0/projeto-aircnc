b15b8fbca356dd88b0863d2e7c7f3851d30fb251
matheusmacedo <matheus.macedo@mhais.com.br>
2019-10-01 20:14:08 -0300

    Install nodemon and create server

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req, res) =>{
-
+    return res.json(
+        {
+            message: "Olá Cliente"
+        })
 });
 
 app.listen(3333);
