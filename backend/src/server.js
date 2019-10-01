3733f17eabbf55c99efe9c832d0dfc9e4a82040c
matheusmacedo <matheus.macedo@mhais.com.br>
2019-10-01 20:57:01 -0300

    Create server

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',(req, res) =>{
-    return res.json(
-        {
-            message: "Olá Cliente"
-        })
+//GET POST PUT DELETE
+
+// req.query = acessar query params do json (para filtros na consulta)
+// req.params = acessar route params (para edição e deletar)
+// req.body = acessar corpo da requisição (para criação e edição)
+
+app.use(express.json());
+app.post('/users',(req, res) =>{
+    return res.json(req.body);
 });
 
 app.listen(3333);
